c03fd4227e85b615ca685ac6d0c0d020888baffb
Andres Pinzon <andrespinzonxd@gmail.com>
2024-07-16 15:23:49 -0500

    feat(back-end): Add Program logic to program.cs

--- AppAsyncronous/Models/Juice.cs
+++ AppAsyncronous/Models/Juice.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Juice
+    public class Juice
     {
     }
 }
--- AppAsyncronous/Models/Toast.cs
+++ AppAsyncronous/Models/Toast.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Toast
+    public class Toast
     {
     }
 }
